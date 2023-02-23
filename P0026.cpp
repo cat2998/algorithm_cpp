0e4b98704cbf9371679643e8b6599dbc95c1bd2d
jihyun <cat2998@naver.com>
2023-02-23 23:46:21 +0900

    02/22

---
+++ P0026.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n, s;
+
+    cin >> n >> s;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    return 0;
+}
