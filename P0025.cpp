b570c901ca3b9122119b15cf78436810f7483be8
jihyun <cat2998@naver.com>
2023-02-27 23:47:20 +0900

    02/27

--- P0025.cpp
+++ P0025.cpp
@@ -5,21 +5,23 @@ using namespace std;
 int find_color(char *chess, int n, int x, int y)
 {
     int white_check = 0;
-    int black_check = 0;
+    // int black_check = 0;
     char white_v[2][8] = { {'W','B','W','B','W','B','W','B'}, {'B','W','B','W','B','W','B','W'} };
-    char black_v[2][8] = { {'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'} };
+    // char black_v[2][8] = { {'B','W','B','W','B','W','B','W'}, {'W','B','W','B','W','B','W','B'} };
 
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
             if (*(chess + x * n + y + i * n + j) == white_v[i % 2][j])
-                black_check++;
-            else if (*(chess + x * n + y + i * n + j) == black_v[i % 2][j])
                 white_check++;
+            // else if (*(chess + x * n + y + i * n + j) == black_v[i % 2][j])
+            //     white_check++;
         }
     }
-    return min(white_check, black_check);
+    // white 시작경우에서 옳지않은 칸들은 black 시작경우에서는 옳은 칸이므로
+    // 체스판전체(64) - white옳지않은칸수(whtie_check)(=black옳은칸수) = black옳지않은칸수
+    return min(white_check, 64 - white_check);
 }
 
 int main(void)
